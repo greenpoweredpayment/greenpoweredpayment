4eb393159e251b3dfc6292a6e49652bcda09fc3b
MrGreenPower <greenpoweredpayment@gmail.com>
2019-02-25 00:13:50 +0100

    Update Ascii.h

--- src/config/Ascii.h
+++ src/config/Ascii.h
@@ -1,25 +1,29 @@
 // Copyright (c) 2018, The TurtleCoin Developers
+// Copyright (c) 2019, GreenPoweredPayment
 //
 // Please see the included LICENSE file for more information
 
 #pragma once
 
 const std::string windowsAsciiArt =
-      "\n _______         _   _       _____      _        \n"
-              "|__   __|       | | | |     / ____|    (_)      \n"
-              "   | |_   _ _ __| |_| | ___| |     ___  _ _ __  \n"
-              "   | | | | | '__| __| |/ _ \\ |    / _ \\| | '_ \\ \n"
-              "   | | |_| | |  | |_| |  __/ |___| (_) | | | | |\n"
-      "   |_|\\__ _|_|   \\__|_|\\___|\\_____\\___/|_|_| |_|\n";
+        "\n _______      ______     ______   \n"
+        "  /______/\    /_____/\   /_____/\  \n"
+        "  \::::__\/__  \:::_ \ \  \:::_ \ \ \n"
+        "   \:\ /____/\  \:(_) \ \  \:(_) \ \\n"
+        "    \:\\_  _\/   \: ___\/   \: ___\/\n"
+        "     \:\_\ \ \    \ \ \      \ \ \  \n"
+        "      \_____\/     \_\/       \_\/  \n"
+        "        -- GreenPoweredPayment --   \n"
 
-const std::string nonWindowsAsciiArt = 
-      "\n                                                                            \n"
-        "████████╗██╗  ██╗██████╗ ████████╗██╗    ██████╗ █████╗ █████╗ ██╗███╗   ██╗\n"
-        "╚══██╔══╝██║  ██║██╔══██╗╚══██╔══╝██║    ██╔═══╝██╔═══╝██╔══██╗██║████╗  ██║\n"
-        "   ██║   ██║  ██║██████╔╝   ██║   ██║    ████╗  ██║    ██║  ██║██║██╔██╗ ██║\n"
-        "   ██║   ██║  ██║██╔══██╗   ██║   ██║    ██╔═╝  ██║    ██║  ██║██║██║╚██╗██║\n"
-        "   ██║   ╚█████╔╝██║  ██║   ██║   ██████╗██████╗╚█████╗╚█████╔╝██║██║ ╚████║\n"
-        "   ╚═╝    ╚════╝ ╚═╝  ╚═╝   ╚═╝   ╚═════╝╚═════╝ ╚════╝ ╚════╝ ╚═╝╚═╝  ╚═══╝\n";
+const std::string nonWindowsAsciiArt =
+        "\n ____     ____     ____    \n"
+        "  /\  _`\  /\  _`\  /\  _`\  \n"
+        "  \ \ \L\_\\ \ \L\ \\ \ \L\ \\n"
+        "   \ \ \L_L \ \ ,__/ \ \ ,__/\n"
+        "    \ \ \/, \\ \ \/   \ \ \/ \n"
+        "     \ \____/ \ \_\    \ \_\ \n"
+        "      \/___/   \/_/     \/_/ \n"
+        "    -- GreenPoweredPayment --\n"
 
 /* Windows has some characters it won't display in a terminal. If your ascii
    art works fine on Windows and Linux terminals, just replace 'asciiArt' with
